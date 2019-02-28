915c60fecc3c5b439051cee4a1cc354bfeca9e73
TRIF3X <thewizardofcrypto@gmail.com>
2019-02-28 14:26:49 -0600

    climbing_stairs with caching

--- climbing_stairs/climbing_stairs.py
+++ climbing_stairs/climbing_stairs.py
@@ -4,11 +4,23 @@
 import sys
 
 def climbing_stairs(n, cache=None):
-  pass
+  if n < 0:
+    return 0
+  elif n == 0:
+    return 1
+  elif cache is None:
+    cache = {i: 0 for i in range(n+1)}
+  elif cache[n] >= 1: 
+    return cache[n]
+  
+  cache[n] = climbing_stairs(n - 1, cache) + climbing_stairs(n - 2, cache) + climbing_stairs(n - 3, cache)
+
+  return cache[n]
 
 
 
-print(climbing_stairs(3))
+
+print(climbing_stairs(4))
 
 if __name__ == "__main__":
   if len(sys.argv) > 1:
